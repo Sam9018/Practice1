73b337454924c88b300b550a87ae738f9b0133ed
Sam Gnuschke <Gnuschke@live.com>
2019-06-04 15:26:19 -0700

    testing commits

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+alert('Hello you');
+    var personsName = prompt('What is your name?');
+    alert('Hi, ' + personsName );
+
+var playGame = confirm('Do you want to play a game?');
+
+if (playGame) {
+  var howOld = prompt('Are you old? Yes or No');
+  var standardhowOld = howOld.toUpperCase();
+  if (standardhowOld === "YES") {
+    alert( 'You\'re so old');
+    } else {
+    alert('You youngin')
+    }
+}
+  //var age = parseInt(howOld);
+  
+  console.log(age, typeof age);
+
+
+    
+var howOld = prompt('Are you old? Yes or No');
+    //var age = parseInt(howOld);
+console.log(age, typeof age);
+
+
+
+console.log(playGame);
