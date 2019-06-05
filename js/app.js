591f2a4e1bac1af0af47f0f3aa20aab0ea542566
Sam Gnuschke <Gnuschke@live.com>
2019-06-04 20:18:16 -0700

    Final commit. Finished JS, CSS , HTML files

--- js/app.js
+++ js/app.js
@@ -5,26 +5,64 @@ alert('Hello you');
     alert('Hi, ' + personsName );
 
 var playGame = confirm('Do you want to play a game?');
-
-if (playGame) {
-  var howOld = prompt('Are you old? Yes or No');
-  var standardhowOld = howOld.toUpperCase();
-  if (standardhowOld === "YES") {
-    alert( 'You\'re so old');
-    } else {
-    alert('You youngin')
-    }
-}
-  //var age = parseInt(howOld);
-  
-  console.log(age, typeof age);
-
-
-    
-var howOld = prompt('Are you old? Yes or No');
-    //var age = parseInt(howOld);
-console.log(age, typeof age);
-
-
-
 console.log(playGame);
+if (playGame) {
+  var howOld = prompt('Am I old? Yes or No');
+  var standardhowOld = howOld.toUpperCase();
+  console.log(howOld);
+  if (standardhowOld === "YES") {
+    alert( 'Incorrect');
+    } else {
+    alert('Correct')
+    }
+ 
+  }
+  if (playGame) {
+    var sports = prompt('Do I play sports? Yes or No');
+    var standardSports = sports.toUpperCase();
+    console.log(standardSports);
+    if (standardSports === "YES") {
+        alert( 'Yeah i do');
+        } else {
+        alert('Got that wrong')
+        }
+
+    }
+
+  if (playGame) {
+    var drive = prompt('Do I drive to school? Yes or No');
+    var standardDrive = drive.toUpperCase();
+    console.log(standardDrive);
+    if (standardDrive === "YES") {
+        alert( 'Yeah i do');
+        } else {
+        alert('Incorrect')
+        }
+
+    }
+
+  if (playGame) {
+    var hair = prompt('Do I have red hair? Yes or No');
+    var standardHair = hair.toUpperCase();
+    console.log(standardHair);
+    if (standardHair === "YES") {
+        alert( 'Correct');
+        } else {
+        alert('Wrong pay more attention')
+        }
+
+    }
+  
+  if (playGame) {
+    var classes = prompt('Am i taking classes? Yes or No');
+    var standardClasses = classes.toUpperCase();
+    console.log(standardClasses);
+    if (standardSports === "YES") {
+        alert( 'Yes i am');
+        } else {
+        alert('Wrong i am taking classes')
+        }
+
+    }
+//var age = parseInt(howOld);
+  
